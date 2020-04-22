f7d6ec681cdb5b09c48cc6cc1258a3d2e2e789ac
Kaiwei Wu <kaiweiwu@Kaiweis-MacBook-Pro.local>
2020-04-22 11:58:34 -0500

    Updates

---
+++ precache-manifest.683ef69dbc48e492b59991d5888fe6cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7613655e921f4f125738cbd73633224e",
+    "url": "/AstroSepc2.0.git/index.html"
+  },
+  {
+    "revision": "359cb4ba826b0fee69b4",
+    "url": "/AstroSepc2.0.git/static/css/2.261927c1.chunk.css"
+  },
+  {
+    "revision": "a2dc0f3086e260f23f7c",
+    "url": "/AstroSepc2.0.git/static/css/main.47c8af2e.chunk.css"
+  },
+  {
+    "revision": "359cb4ba826b0fee69b4",
+    "url": "/AstroSepc2.0.git/static/js/2.d25fc2a0.chunk.js"
+  },
+  {
+    "revision": "f47ec2953cceb72a570cb66d68ca1cad",
+    "url": "/AstroSepc2.0.git/static/js/2.d25fc2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2dc0f3086e260f23f7c",
+    "url": "/AstroSepc2.0.git/static/js/main.ed6062ae.chunk.js"
+  },
+  {
+    "revision": "d59436f07c41f4afca80",
+    "url": "/AstroSepc2.0.git/static/js/runtime-main.39405415.js"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/AstroSepc2.0.git/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/AstroSepc2.0.git/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/AstroSepc2.0.git/static/media/dxicons.2b145cef.woff"
+  }
+]);
